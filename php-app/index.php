37d0536d560fd58c1b658b36fb9c63e95a442d70
eddie-chang <eddie.chang@nextlink.com.tw>
2017-03-20 12:23:03 +0800

    Update index.php

--- php-app/index.php
+++ php-app/index.php
@@ -90,6 +90,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <body id="sample">
   <div class="textColumn">
     <h1>Congratulations!</h1>
+    <h2>v 1.0.1</h2>
     <p>Your Docker Container is now running in Elastic Beanstalk on your own dedicated environment in the AWS Cloud.</p>
   </div>
   
